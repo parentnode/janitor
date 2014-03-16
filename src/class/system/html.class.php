c36111f77b0c11f548da720b70395e72846342b0
kaestel <kaestel@e-types.com>
2014-03-16 14:05:54 +0100

    Select options as simple array + Cleanup + comments

--- src/class/system/html.class.php
+++ src/class/system/html.class.php
@@ -9,8 +9,9 @@ class HTML {
 
 	/**
 	* Make html tag attribute
-	* Classes passed as speparate parameters
-	* if class = false, it is not added.
+	* Attribute values passed as speparate parameters
+	* if value is false, it is not added.
+	* if attribute has no value, empty string is returned
 	* 
 	* @param string $attribute_name Name of attribute.
 	* @param strings Optional strings to become value of attribute.
@@ -37,6 +38,19 @@ class HTML {
 
 
 	/**
+	* WHAT TO DO ABOUT CUSTOM FIELDS
+	* Is it separate fields or one field with two inputs? 
+	* It gets complicated, what are pros and cons
+	* Consider thoroughly before adding a custom field
+	*
+	* When building model, pros:
+	* - declare only one input
+	*
+	* When building model, cons:
+	* - less flexibily in organizing HTML
+	* - new variables to set name and value
+	*
+	*
 	* Basic input element
 	*
 	* @return string Input element
@@ -46,81 +60,101 @@ class HTML {
 		// print_r($this->data_entities);
 		// print "</p>";
 
-		$type = $this->getEntityProperty($name, "type");
-		$value = $this->getEntityProperty($name, "value");
-//		print "###".$value."###";
 
+		// Get default settings from model first
+
+		// label
 		$label = $this->getEntityProperty($name, "label");
 
+		// type, value and options
+		$type = $this->getEntityProperty($name, "type");
+		$value = $this->getEntityProperty($name, "value");
+		$options = $this->getEntityProperty($name, "options");
+
+		// input state
+		$readonly = $this->getEntityProperty($name, "readonly");
+		$disabled = $this->getEntityProperty($name, "disabled");
+		$checked = $this->getEntityProperty($name, "checked");
+
+		// frontend stuff
+		// TODO: check why class does not get model classname?
 		$class = false;
 		$id = $this->getEntityProperty($name, "id");
+
+		// validation
 		$min = $this->getEntityProperty($name, "min");
 		$max = $this->getEntityProperty($name, "max");
 		$required = $this->getEntityProperty($name, "required");
 		$pattern = $this->getEntityProperty($name, "pattern");
 
+		// visual feedback
 		$hint_message = $this->getEntityProperty($name, "hint_message");
 		$error_message = $this->getEntityProperty($name, "error_message");
 
+
+		// custom fields
+		// see note above before adding more custom fields
+
 		// price specifics
-		$currency = $this->getEntityProperty($name, "currency");
-		$vatrate = $this->getEntityProperty($name, "vatrate");
+		if(!$type || $type == "geolocation") {
+			$currency = $this->getEntityProperty($name, "currency");
+			$vatrate = $this->getEntityProperty($name, "vatrate");
+		}
 
 
-		$options = $this->getEntityProperty($name, "options");
-		$checked = $this->getEntityProperty($name, "checked");
-
+		// overwrite defaults
 		if($_options !== false) {
 			foreach($_options as $_option => $_value) {
 				switch($_option) {
 
+					case "label"           : $label            = $_value; break;
 					case "type"            : $type             = $_value; break;
 					case "value"           : $value            = $_value; break;
-					case "label"           : $label            = $_value; break;
-					case "required"        : $required         = $_value; break;
-					case "pattern"         : $pattern          = $_value; break;
+					case "options"         : $options          = $_value; break;
 
-					case "min"             : $min              = $_value; break;
-					case "max"             : $max              = $_value; break;
-
-					case "error_message"   : $error_message    = $_value; break;
-					case "hint_message"    : $hint_message     = $_value; break;
+					case "readonly"        : $readonly         = $_value; break;
+					case "disabled"        : $disabled         = $_value; break;
+					case "checked"         : $checked          = $_value; break;
 
 					case "class"           : $class            = $_value; break;
 					case "id"              : $id               = $_value; break;
 
+					case "min"             : $min              = $_value; break;
+					case "max"             : $max              = $_value; break;
+					case "required"        : $required         = $_value; break;
+					case "pattern"         : $pattern          = $_value; break;
+
+					case "error_message"   : $error_message    = $_value; break;
+					case "hint_message"    : $hint_message     = $_value; break;
+
+
 					case "currency"        : $currency         = $_value; break;
 					case "vatrate"         : $vatrate          = $_value; break;
-
-					case "options"         : $options          = $_value; break;
-
-					case "checked"         : $checked          = $_value; break;
 
 				}
 			}
 		}
 
+		// Start generating HTML
 
 		$_ = '';
 
 		$for = stringOr($id, "input_".$name);
 		$att_id = $this->attribute("id", $for);
-
 		$att_name = $this->attribute("name", $name);
 
-		$att_disabled = strstr($class, "disabled") ? $this->attribute("disabled", "disabled") : '';
-		$att_readonly = strstr($class, "readonly") ? $this->attribute("readonly", "readonly") : '';
+		$att_disabled = $disabled ? $this->attribute("disabled", "disabled") : '';
+		$att_readonly = $readonly ? $this->attribute("readonly", "readonly") : '';
 
+		$att_class = $this->attribute("class", "field", $type, $class, ($required ? "required" : ""), ($disabled ? "disabled" : ""), ($readonly ? "readonly" : ""), ($min ? "min:".$min : ""), ($max ? "max:".$max : ""));
 
-		$att_class = $this->attribute("class", "field", $type, $class, ($required ? "required" : ""), ($min ? "min:".$min : ""), ($max ? "max:".$max : ""));
 
 		// attribute strips value for slashes etc - cannot be used for patterns
 		$att_pattern = $pattern ? ' pattern="'.$pattern.'"' : '';
 
-
+		// multiple selects?
 		$att_multiple = $this->attribute("multiple", ($max && $max > 1 ? "multiple" : ""));
 
-		// strings length
 
 		// hidden field
 		if($type == "hidden") {
@@ -131,19 +165,27 @@ class HTML {
 
 		$_ .= '<div'.$att_class.'>';
 
+
+			// INPUT TYPES:
+			// checkboxes and radiobuttons have label after input
+
+
 			// CHECKBOX/BOOLEAN
-			// checkbox in front of label
 			if($type == "checkbox" || $type == "boolean") {
 				$att_value = $this->attribute("value", $value);
 				$att_name = $this->attribute("name", $name);
 				$att_checked = $this->attribute("checked", $checked);
 
+				// fallback hidden input so checkbox always sends value (even when not checked)
 				$_ .= '<input type="hidden"'.$att_name.' value="0" />';
 				$_ .= '<input type="checkbox"'.$att_name.$att_id.$att_value.$att_checked.$att_disabled.$att_readonly.$att_pattern.' />';
 			}
 
 
 			// TODO: Add radio output
+			if($type == "radio") {
+				$_ .= 'RADIO BUTTONS NOT IMPLEMENTED IN HTML.CLASS';
+			}
 
 
 			// LABEL
@@ -224,6 +266,19 @@ class HTML {
 				$_ .= '<textarea'.$att_name.$att_id.$att_disabled.$att_readonly.$att_max.'>'.$value.'</textarea>';
 			}
 
+
+			// SELECT
+			// TODO: Refine select output
+			else if($type == "select") {
+
+				$_ .= '<select'.$att_name.$att_id.$att_disabled.$att_readonly.'>';
+				foreach($options as $select_option => $select_value) {
+					$_ .= '<option value="'.$select_option.'"'.($value == $select_option ? ' selected="selected"' : '').'>'.$select_value.'</option>';
+				}
+				$_ .= '</select>';
+			}
+
+
 			// TAGS 
 			else if($type == "tags") {
 				$att_name = $this->attribute("name", $name);
@@ -240,16 +295,6 @@ class HTML {
 				$_ .= '<input type="hidden" value="'.$vatrate.'" name="vatrate" />';
 			}
 
-			// SELECT
-			// TODO: Refine select output
-			else if($type == "select") {
-
-				$_ .= '<select'.$att_name.$att_id.$att_disabled.$att_readonly.'>';
-				foreach($options as $select_option => $select_value) {
-					$_ .= '<option value="'.$select_option.'"'.($value == $select_option ? ' selected="selected"' : '').'>'.$select_value.'</option>';
-				}
-				$_ .= '</select>';
-			}
 
 			// HINT AND ERROR MESSAGE
 			$_ .= '<div'.$this->attribute("class", "help").'>';
@@ -262,6 +307,7 @@ class HTML {
 
 		return $_;
 	}
+
 
 	/**
 	* Basic input type="submit" element
