2e1911088c566ff52a4273f65bcc653bbfb61e59
kaestel <kaestel@e-types.com>
2014-05-20 16:26:33 +0200

    Updated janitor to Manipulator v0.8

--- src/www/js/lib/seg_desktop_ie_include.js
+++ src/www/js/lib/seg_desktop_ie_include.js
@@ -1,8 +1,5 @@
-document.write('<script type="text/javascript" src="/janitor/js/manipulator/v0_7_5/full/seg_desktop_ie.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/js/manipulator/v0_8/full/seg_desktop_ie.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-form.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-navigation.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-sortable.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-audio.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-video.js"></script>');
--- src/www/js/lib/seg_desktop_light_include.js
+++ src/www/js/lib/seg_desktop_light_include.js
@@ -1,2 +1,1 @@
-document.write('<script type="text/javascript" src="/js/manipulator/v0_7_5/full/seg_desktop.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/js/lib/i-unsupported.js"></script>');
