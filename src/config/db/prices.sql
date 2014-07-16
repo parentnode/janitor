e55895c5cf1d5358c725db7ddd13e86fb0758733
kaestel <kaestel@e-types.com>
2014-07-16 12:38:55 +0200

    Simplified prices

--- src/config/db/prices.sql
+++ src/config/db/prices.sql
@@ -9,9 +9,7 @@ CREATE TABLE `prices` (
   PRIMARY KEY  (`id`),
   KEY `item_id` (`item_id`),
   KEY `currency` (`currency`),
-  KEY `vatrate_id` (`vatrate_id`),
 
   CONSTRAINT `prices_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `prices_ibfk_2` FOREIGN KEY (`currency`) REFERENCES `currencies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `prices_ibfk_3` FOREIGN KEY (`vatrate_id`) REFERENCES `vatrates` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `prices_ibfk_2` FOREIGN KEY (`currency`) REFERENCES `currencies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
