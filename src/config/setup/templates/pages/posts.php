125bf56329286662b54506c7715c59215e7bc6b4
kaestel <kaestel@e-types.com>
2014-08-01 13:06:15 +0200

    Added pagination class

--- src/config/setup/templates/pages/posts.php
+++ src/config/setup/templates/pages/posts.php
@@ -4,76 +4,41 @@ global $action;
 
 $itemtype = "post";
 
-$count = stringOr(getVar("count"), 5);
 
 // get post tags for listing
 $post_tags = $IC->getTags(array("context" => $itemtype));
 
 
-// get all items as base
-$items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1));
+// get content pagination
+include_once("class/items/pagination.class.php");
+$PC = new Pagination();
 
-# /blog - lists the latest N posts and prev button
-if(!isset($action[0])) {
+$limit = stringOr(getVar("limit"), 5);
+$sindex = isset($action[0]) ? $action[0] : false;
+$direction = isset($action[1]) ? $action[1] : false; 
 
-	$range_items = $IC->getItems(array("itemtype" => $itemtype, "status" => 1, "limit" => $count));
-}
-
-# /blog/#sindex#[/prev|next]
-else if(isset($action[0])) {
-
-	$item_id = $IC->getIdFromSindex($action[0]);
-
-	# /blog/#sindex#/next - Lists the next N posts after sindex
-	if(isset($action[1]) && $action[1] == "next") {
-
-		$range_items = $IC->getNext($item_id, array("items" => $items, "count" => $count));		
-	}
-	# /blog/#sindex#/prev - Lists the prev N posts before sindex
-	else if(isset($action[1]) && $action[1] == "prev") {
-
-		$range_items = $IC->getPrev($item_id, array("items" => $items, "count" => $count));
-	}
-	# /blog/#sindex# - Lists the next N posts starting with sindex
-	else {
-
-		$item = $IC->getItem(array("id" => $item_id));
-		$range_items = $IC->getNext($item_id, array("items" => $items, "count" => $count-1));
-
-		array_unshift($range_items, $item);
-	}
-
-}
-
-// find indexes and ids for next/prev
-$first_id = isset($range_items[0]) ? $range_items[0]["id"] : false;
-$first_sindex = isset($range_items[0]) ? $range_items[0]["sindex"] : false;
-$last_id = isset($range_items[count($range_items)-1]) ? $range_items[count($range_items)-1]["id"] : false;
-$last_sindex = isset($range_items[count($range_items)-1]) ? $range_items[count($range_items)-1]["sindex"] : false;
-
-// look for next/prev item availability
-$next = $last_id ? $IC->getNext($last_id, array("items" => $items)) : false;
-$prev = $first_id ? $IC->getPrev($first_id, array("items" => $items)) : false;
+$pattern = array("itemtype" => $itemtype, "status" => 1);
+$pagination = $PC->paginate(array("pattern" => $pattern, "sindex" => $sindex, "limit" => $limit, "direction" => $direction));
 
 ?>
 
 <div class="scene posts i:generic">
 	<h1>Janitor is alive</h1>
 
-	<div class="categories">
 <?	if($post_tags): ?>
+	<div class="categories">
 		<h2>Categories</h2>
 		<ul class="tags">
 <?		foreach($post_tags as $tag): ?>
-			<li><a href="/blog/tag/<?= urlencode($tag["value"]) ?>"><?= $tag["value"] ?></a></li>
+			<li><a href="/index/tag/<?= urlencode($tag["value"]) ?>"><?= $tag["value"] ?></a></li>
 <?		endforeach; ?>
 		</ul>
-<?	endif; ?>
 	</div>
+<?	endif; ?>
 
-<? if($range_items): ?>
+<? if($pagination["range_items"]): ?>
 	<ul class="postings">
-<?		foreach($range_items as $item):
+<?		foreach($pagination["range_items"] as $item):
 			$item = $IC->extendItem($item, array("tags" => true)); ?>
 		<li class="item post id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/Article">
 
@@ -107,11 +72,11 @@ $prev = $first_id ? $IC->getPrev($first_id, array("items" => $items)) : false;
 <? endif; ?>
 
 
-<? if($next || $prev): ?>
+<? if($pagination["next"] || $pagination["prev"]): ?>
 	<div class="pagination">
 		<ul class="actions">
-<? if($prev): ?><li class="previous"><a href="/blog/<?= $first_sindex ?>/prev">Previous page</a></li><? endif; ?>
-<? if($next): ?><li class="next"><a href="/blog/<?= $last_sindex ?>/next">Next page</a></li><? endif; ?>
+<? if($pagination["prev"]): ?><li class="previous"><a href="/blog/<?= $pagination["first_sindex"] ?>/prev">Previous page</a></li><? endif; ?>
+<? if($pagination["next"]): ?><li class="next"><a href="/blog/<?= $pagination["last_sindex"] ?>/next">Next page</a></li><? endif; ?>
 		</ul>
 	</div>
 <? endif; ?>
