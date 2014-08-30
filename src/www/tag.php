3dc85b921c6c355716ad1accb0a9a30fc521fb26
kaestel <kaestel@e-types.com>
2014-08-30 19:33:01 +0200

    Updated visual interface

--- src/www/tag.php
+++ src/www/tag.php
@@ -1,24 +1,17 @@
 <?php
 $access_item["/list/"] = true;
 $access_item["/edit/"] = true;
-$access_item["/globalUpdateTag/"] = true;
-$access_item["/globalDeleteTag/"] = true;
-
-
+$access_item["/updateTag/"] = true;
+$access_item["/deleteTag/"] = true;
 if(isset($read_access) && $read_access) {
 	return;
 }
 
-
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
-// include the output class for output method support
-include_once("class/system/output.class.php");
 
 $action = $page->actions();
-
-$IC = new Item();
-$output = new Output();
+$model = new Tag();
 
 
 $page->bodyClass("tags");
@@ -27,41 +20,46 @@ $page->pageTitle("Tags management");
 
 if(is_array($action) && count($action)) {
 
-	if(preg_match("/[a-zA-Z]+/", $action[0]) && $page->validateCsrfToken()) {
-
-		// check if custom function exists on User class
-		if($IC && method_exists($IC, $action[0])) {
-
-			$output->screen($IC->$action[0]($action));
-			exit();
-		}
-	}
-
 	// LIST ITEM
-	// Requires exactly two parameters /enable/#item_id#
 	if(count($action) == 1 && $action[0] == "list") {
 
-		$page->header(array("type" => "admin"));
-		$page->template("admin/tag/list.php");
-		$page->footer(array("type" => "admin"));
+		$page->page(array(
+			"type" => "admin",
+			"templates" => "admin/tag/list.php"
+			)
+		);
 		exit();
 
 	}
 	// EDIT ITEM
 	else if(count($action) == 2 && $action[0] == "edit") {
-	
-		$page->header(array("type" => "admin"));
-		$page->template("admin/tag/edit.php");
-		$page->footer(array("type" => "admin"));
+
+		$page->page(array(
+			"type" => "admin",
+			"templates" => "admin/tag/edit.php"
+			)
+		);
 		exit();
 	
 	}
 
+	// Class interface
+	else if($page->validateCsrfToken() && preg_match("/[a-zA-Z]+/", $action[0])) {
+
+		// check if custom function exists on User class
+		if($model && method_exists($model, $action[0])) {
+
+			$output = new Output();
+			$output->screen($model->$action[0]($action));
+			exit();
+		}
+	}
 
 }
 
-$page->header();
-$page->template("404.php");
-$page->footer();
+$page->page(array(
+	"templates" => "pages/404.php"
+	)
+);
 
 ?>
