e56cf6db99ed822817175aca540b84b29f1c47b3
kaestel <kaestel@e-types.com>
2014-08-30 19:27:11 +0200

    Updated address sql

--- src/config/db/user_addresses.sql
+++ src/config/db/user_addresses.sql
@@ -12,11 +12,13 @@ CREATE TABLE `SITE_DB`.`user_addresses` (
   `city` varchar(255) DEFAULT NULL,
   `postal` varchar(255) DEFAULT NULL,
   `state` varchar(255) DEFAULT NULL,
-  `country` varchar(255) DEFAULT NULL,
+  `country` varchar(2) DEFAULT NULL,
 
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `item_addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
+  KEY `country` (`country`),
+  CONSTRAINT `item_addresses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT `item_addresses_ibfk_2` FOREIGN KEY (`country`) REFERENCES `countries` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
