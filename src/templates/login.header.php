ca3a95e800f75a648c06c73c1336a087c248d617
kaestel <kaestel@e-types.com>
2014-09-11 12:56:31 +0200

    fixed forced dev include

--- src/templates/login.header.php
+++ src/templates/login.header.php
@@ -11,7 +11,7 @@
 	<meta name="viewport" content="initial-scale=1, user-scalable=no" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-<? if(1 || session()->value("dev")) { ?>
+<? if(session()->value("dev")) { ?>
 	<link type="text/css" rel="stylesheet" media="all" href="/admin/css/lib/seg_<?= $this->segment() ?>_include.css" />
 	<script type="text/javascript" src="/admin/js/lib/seg_<?= $this->segment() ?>_include.js"></script>
 <? } else { ?>
