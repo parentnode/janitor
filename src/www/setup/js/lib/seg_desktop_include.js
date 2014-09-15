4c2069ae0d25ea08d96abfa89ec4dd5726ebbbe3
kaestel <kaestel@e-types.com>
2014-09-15 20:38:53 +0200

    setup js+css merged

--- src/www/setup/js/lib/seg_desktop_include.js
+++ src/www/setup/js/lib/seg_desktop_include.js
@@ -1,12 +1,5 @@
 document.write('<script type="text/javascript" src="/janitor/js/lib/seg_desktop_include.js"></script>');
 
-// document.write('<script type="text/javascript" src="/js/manipulator/v0_8/full/seg_desktop.js"></script>');
-//
-// document.write('<script type="text/javascript" src="/js/manipulator/v0_8/src/u-events-browser.js"></script>');
-// document.write('<script type="text/javascript" src="/js/manipulator/v0_8/src/u-form.js"></script>');
-//
-// document.write('<script type="text/javascript" src="/js/lib/desktop/u-notifier.js"></script>');
-
 document.write('<script type="text/javascript" src="/setup/js/lib/desktop/i-page-desktop.js"></script>');
 
 document.write('<script type="text/javascript" src="/setup/js/lib/desktop/i-progress-desktop.js"></script>');
