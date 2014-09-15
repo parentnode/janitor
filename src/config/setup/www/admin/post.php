250773dfe3e19cb323bf165af35e365ca807a43b
kaestel <kaestel@e-types.com>
2014-09-15 13:26:01 +0200

    Updated controller to new standard

--- src/config/setup/www/admin/post.php
+++ src/config/setup/www/admin/post.php
@@ -6,16 +6,11 @@ if(isset($read_access) && $read_access) {
 
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
-// include the output class for output method support
-include_once("class/system/output.class.php");
 
 $action = $page->actions();
-
 $IC = new Item();
 $itemtype = "post";
-
 $model = $IC->typeObject($itemtype);
-$output = new Output();
 
 
 $page->bodyClass($itemtype);
@@ -24,50 +19,57 @@ $page->pageTitle("Posts");
 
 if(is_array($action) && count($action)) {
 
-	if(preg_match("/[a-zA-Z]+/", $action[0]) && $page->validateCsrfToken()) {
-
-		// check if custom function exists on User class
-		if($model && method_exists($model, $action[0])) {
-
-			$output->screen($model->$action[0]($action));
-			exit();
-		}
-	}
-
 	// LIST ITEM
-	// Requires exactly two parameters /enable/#item_id#
 	if(count($action) == 1 && $action[0] == "list") {
 
-		$page->header(array("type" => "admin"));
-		$page->template("admin/".$itemtype."/list.php");
-		$page->footer(array("type" => "admin"));
+		$page->page(array(
+			"type" => "admin",
+			"templates" => "admin/".$itemtype."/list.php"
+			)
+		);
 		exit();
 
 	}
 	// NEW ITEM
 	else if(count($action) == 1 && $action[0] == "new") {
 
-		$page->header(array("type" => "admin"));
-		$page->template("admin/".$itemtype."/new.php");
-		$page->footer(array("type" => "admin"));
+		$page->page(array(
+			"type" => "admin",
+			"templates" => "admin/".$itemtype."/new.php"
+			)
+		);
 		exit();
 
 	}
-	// NEW ITEM
+	// EDIT ITEM
 	else if(count($action) == 2 && $action[0] == "edit") {
-	
-		$page->header(array("type" => "admin"));
-		$page->template("admin/".$itemtype."/edit.php");
-		$page->footer(array("type" => "admin"));
+
+		$page->page(array(
+			"type" => "admin",
+			"templates" => "admin/".$itemtype."/edit.php"
+			)
+		);
 		exit();
-	
+
 	}
 
+	// Class interface
+	else if($page->validateCsrfToken() && preg_match("/[a-zA-Z]+/", $action[0])) {
+
+		// check if custom function exists on User class
+		if($model && method_exists($model, $action[0])) {
+
+			$output = new Output();
+			$output->screen($model->$action[0]($action));
+			exit();
+		}
+	}
 
 }
 
-$page->header();
-$page->template("404.php");
-$page->footer();
+$page->page(array(
+	"templates" => "pages/404.php"
+	)
+);
 
 ?>
