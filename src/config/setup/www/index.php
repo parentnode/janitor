4dd2aff4e15c43a9c3149d9bd9a3be9e8f219011
kaestel <kaestel@e-types.com>
2014-09-15 13:26:17 +0200

    Updated controller to new standard

--- src/config/setup/www/index.php
+++ src/config/setup/www/index.php
@@ -6,41 +6,35 @@ if(isset($read_access) && $read_access) {
 
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 
-// include the output class for output method support
-include_once("class/system/output.class.php");
 
 $action = $page->actions();
 
-$IC = new Item();
-$output = new Output();
-
 
 $page->bodyClass("post");
-$page->pageTitle("Post");
+$page->pageTitle("Posts");
 
 
-if(is_array($action) && count($action)) {
 
-	if(preg_match("/[a-zA-Z\\-_]+/", $action[0])) {
-		if(is_array($action) && count($action)) {
+// news list for tags
+// /blog/tag/#tag#
+// /blog/tag/#tag#/#sindex#/prev|next
+if(count($action) >= 2 && $action[0] == "tag") {
 
-			# /blog/tag/#tag#[/#sindex#/prev|next]
-			if(count($action) > 1 && $action[0] == "tag") {
-
-				$page->header();
-				$page->template("pages/posts_tag.php");
-				$page->footer();
-				exit();
-
-			}
-		}
-	}
+	$page->page(array(
+		"templates" => "pages/posts_tag.php"
+		)
+	);
+	exit();
 
 }
 
-$page->header();
-$page->template("pages/posts.php");
-$page->footer();
+
+$page->page(array(
+	"templates" => "pages/posts.php"
+	)
+);
+exit();
+
 
 ?>
  
