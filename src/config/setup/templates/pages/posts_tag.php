eafbd6f0b933787d206a436b8db49293858cd164
kaestel <kaestel@e-types.com>
2014-09-23 13:33:55 +0200

    Updated setup default files

--- src/config/setup/templates/pages/posts_tag.php
+++ src/config/setup/templates/pages/posts_tag.php
@@ -1,11 +1,12 @@
 <?php
-global $IC;
 global $action;
 
+
+$IC = new Item();
 $itemtype = "post";
+$tag = urldecode($action[1]);
 
-
-// get post tags for listing
+// get tags for filters
 $post_tags = $IC->getTags(array("context" => $itemtype));
 
 
@@ -13,31 +14,21 @@ $post_tags = $IC->getTags(array("context" => $itemtype));
 include_once("class/items/pagination.class.php");
 $PC = new Pagination();
 
-$limit = stringOr(getVar("limit"), 5);
-$sindex = isset($action[0]) ? $action[0] : false;
-$direction = isset($action[1]) ? $action[1] : false; 
+$limit = stringOr(getVar("limit"), 6);
+$sindex = isset($action[2]) ? $action[2] : false;
+$direction = isset($action[3]) ? $action[3] : false; 
 
-$pattern = array("itemtype" => $itemtype, "status" => 1);
+$pattern = array("itemtype" => $itemtype, "status" => 1, "tags" => $itemtype.":".addslashes($tag), "order" => "published_at DESC");
 $pagination = $PC->paginate(array("pattern" => $pattern, "sindex" => $sindex, "limit" => $limit, "direction" => $direction));
 
 ?>
 
-<div class="scene posts i:generic">
-	<h1>Janitor is alive</h1>
+<div class="scene posts tag i:generic">
+	<h1><?= $tag ?></h1>
 
-<?	if($post_tags): ?>
-	<div class="categories">
-		<h2>Categories</h2>
-		<ul class="tags">
-<?		foreach($post_tags as $tag): ?>
-			<li><a href="/index/tag/<?= urlencode($tag["value"]) ?>"><?= $tag["value"] ?></a></li>
-<?		endforeach; ?>
-		</ul>
-	</div>
-<?	endif; ?>
+<?	if($pagination["range_items"]): ?>
 
-<? if($pagination["range_items"]): ?>
-	<ul class="postings">
+	<ul class="postings i:articlelist">
 <?		foreach($pagination["range_items"] as $item):
 			$item = $IC->extendItem($item, array("tags" => true));
 			$hardlink = (isset($_SERVER["HTTPS"]) ? "https" : "http")."://".$_SERVER["SERVER_NAME"]."/blog/tag/".$tag."/".$item["sindex"];
@@ -58,7 +49,6 @@ $pagination = $PC->paginate(array("pattern" => $pattern, "sindex" => $sindex, "l
 <?				endforeach; ?>
 <?			endif; ?>
 			</ul>
-
 			<h2 itemprop="name"><?= $item["name"] ?></h2>
 
 			<dl class="info">
@@ -89,11 +79,24 @@ $pagination = $PC->paginate(array("pattern" => $pattern, "sindex" => $sindex, "l
 <? if($pagination["next"] || $pagination["prev"]): ?>
 	<div class="pagination">
 		<ul class="actions">
-<? if($pagination["prev"]): ?><li class="previous"><a href="/blog/<?= $pagination["first_sindex"] ?>/prev">Previous page</a></li><? endif; ?>
-<? if($pagination["next"]): ?><li class="next"><a href="/blog/<?= $pagination["last_sindex"] ?>/next">Next page</a></li><? endif; ?>
+<? if($pagination["prev"]): ?><li class="previous"><a href="/blog/tag/<?= $tag ?>/<?= $pagination["first_sindex"] ?>/prev">Previous page</a></li><? endif; ?>
+<? if($pagination["next"]): ?><li class="next"><a href="/blog/tag/<?= $tag ?>/<?= $pagination["last_sindex"] ?>/next">Next page</a></li><? endif; ?>
 		</ul>
 	</div>
 <? endif; ?>
 
+
+<?	if($post_tags): ?>
+	<h2>Categories</h2>
+	<ul class="tags">
+<?		foreach($post_tags as $tag): ?>
+		<li><a href="/blog/tag/<?= urlencode($tag["value"]) ?>"><?= $tag["value"] ?></a></li>
+<?		endforeach; ?>
+	</ul>
+<?	endif; ?>
+
+	<ul class="actions">
+		<li class="more"><a href="/blog">All postings</a></li>
+	</ul>
 
 </div>
