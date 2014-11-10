8e47efd52fc8a9fbff3e2dbb8bf705d72a6b2762
kaestel <kaestel@e-types.com>
2014-11-10 23:34:53 +0000

    Merged js+css

--- src/www/js/lib/seg_desktop_ie_include.js
+++ src/www/js/lib/seg_desktop_ie_include.js
@@ -9,15 +9,17 @@ document.write('<script type="text/javascript" src="/js/manipulator/src/u-reques
 document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-sortable.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-keyboard.js"></script>');
 
+
 document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/src/beta-u-audio.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/src/beta-u-video.js"></script>');
-document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/src/beta-u-keys.js"></script>');
+
 
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-page.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-form.js"></script>');
 
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultlist.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultedit.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultnew.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaulteditstatus.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaulteditactions.js"></script>');
 
@@ -25,13 +27,6 @@ document.write('<script type="text/javascript" src="/janitor/admin/js/lib/deskto
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultmedia.js"></script>');
 
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-navigations.js"></script>');
-
-
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-form_defaultnew.js"></script>');
-
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-form_defaultstatus.js"></script>');
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-form_defaultdelete.js"></script>');
-
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-users.js"></script>');
 
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/u-notifier.js"></script>');
---
+++ src/www/js/seg_tv.js
@@ -0,0 +1,9 @@
+
+/*seg_tv_include.js*/
+
+/*i-unsupported.js*/
+function unsupported() {
+	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333">Your browser is NOT supported. It is more outdated than steam-engines, typewriters and VHS tapes, so stop acting surprised.</h1><p style="text-align: center; margin: 0 10%; font-family: Arial; color: #333333">or<br />(you changed your UserAgent)<br />(we are working on it)<br />(we made an error - <a href="mailto:martin@parentnode.dk">let us know</a>)</p>';
+}
+window.onload = unsupported;
+
