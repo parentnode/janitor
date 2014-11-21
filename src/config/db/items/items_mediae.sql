9a1546d070e57687c54e97875b669e3e9a980d09
kaestel <kaestel@e-types.com>
2014-11-21 09:13:03 +0000

    items_mediae

---
+++ src/config/db/items/items_mediae.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `SITE_DB`.`items_mediae` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` int(11) NOT NULL,
+
+  `name` varchar(255) NOT NULL,
+  `format` varchar(5) NOT NULL,
+  `variant` varchar(13) NOT NULL,
+  `width` int(11) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `filesize` int(11) DEFAULT NULL,
+  `position` int(11) NOT NULL,
+
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`),
+  CONSTRAINT `items_mediae_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
