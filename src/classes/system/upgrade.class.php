b93fba6fce8257b4755b23f88a5144763e212bfc
kaestel <kaestel@e-types.com>
2014-12-01 13:47:07 +0100

    New upgrade helper

--- src/classes/system/upgrade.class.php
+++ src/classes/system/upgrade.class.php
@@ -11,9 +11,93 @@ class Upgrade {
 
 
 
+	// 0.7 to v 0.9 UPGRADE HELPERS
+
+
+	function moveFilesColumnToItems($itemtype, $variant = false, $column = "files") {
+
+		$query = new Query();
+		$IC = new Items();
+		$fs = new FileSystem();
+
+		$query->checkDbExistance(UT_ITEMS_MEDIAE);
+
+
+		$sql = "SELECT * FROM ".SITE_DB.".item_".$itemtype;
+		$query->sql($sql);
+		$results = $query->results();
+
+//		print_r($results);
+
+		foreach($results as $result) {
+
+		//	print $result["files"] . "<br>";
+			if($result[$column]) {
+
+				$item_id = $result["item_id"];
+				$format = $result[$column];
+
+				if(file_exists(PRIVATE_FILE_PATH."/".$item_id."/".$format)) {
+					$file = PRIVATE_FILE_PATH."/".$item_id."/".$format;
+				}
+				else if(file_exists(PRIVATE_FILE_PATH."/".$item_id."/".$variant."/".$format)) {
+					$file = PRIVATE_FILE_PATH."/".$item_id."/".$variant."/".$format;
+				}
+				else {
+					continue;
+				}
+
+				$new_file = PRIVATE_FILE_PATH."/".$item_id."/".$variant."/".$format;
+
+				if(preg_Match("/^(png|jpg)$/", $format)) {
+					$image = new Imagick($file);
+
+					// check if we can get relevant info about image
+					$width = $image->getImageWidth();
+					$height = $image->getImageHeight();
+				}
+				else if(preg_Match("/^(mov|mp4)$/", $format)) {
+					include_once("classes/system/video.class.php");
+					$VC = new Video();
+					$info = $VC->info($file);
+					$width = $info["width"];
+					$height = $info["height"];
+				}
+				else {
+					$width = 0;
+					$height = 0;
+				}
+
+				$variant = $variant ? $variant : randomKey(8);
+				$name = $format;
+				$filesize = filesize($file);
+
+				// insert into new table
+				$sql = "INSERT INTO ".UT_ITEMS_MEDIAE." VALUES(DEFAULT, $item_id, '$name', '$format', '$variant', '$width', '$height', '$filesize', 0)";
+//				print $sql."<br>";
+
+				$query->sql($sql);
+
+				// move image to new folder
+				$fs->makeDirRecursively(dirname($new_file));
+
+				if($file != $new_file) {
+					copy($file, PRIVATE_FILE_PATH."/".$item_id."/".$variant."/".$format);
+					unlink(PRIVATE_FILE_PATH."/".$item_id."/".$format);
+				}
+				$fs->removeDirRecursively(PUBLIC_FILE_PATH."/".$item_id);
+
+			}
+		}
+		print "You can now delete '$column' column in ".SITE_DB.".item_".$itemtype."<br>";
+
+	}
+
+
+
 	// V 0.7 to v 0.8 UPGRADE HELPERS
 
-	function moveMediaeToItems($itemtype) {
+	function moveMediaeToItems($itemtype, $variant) {
 
 
 		$query = new Query();
