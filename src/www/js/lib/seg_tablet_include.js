027e016981b96e51965475c954fc914059b0a2bf
kaestel <kaestel@e-types.com>
2014-12-26 11:25:36 +0300

    Mobile backend support, first draft

--- src/www/js/lib/seg_tablet_include.js
+++ src/www/js/lib/seg_tablet_include.js
@@ -1,1 +1,47 @@
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/i-unsupported.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/full/seg_tablet.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-debug.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-events-browser.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-builder.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-htmleditor.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-geolocation.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-geometry.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-dom.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-request.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-sortable.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-keyboard.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-audio.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-video.js"></script>');
+
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-history.js"></script>');
+document.write('<script type="text/javascript" src="/js/manipulator/src/u-navigation.js"></script>');
+
+
+// document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/src/beta-u-audio.js"></script>');
+// document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/src/beta-u-video.js"></script>');
+
+
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-page.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-form.js"></script>');
+
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultlist.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultedit.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultnew.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaulteditstatus.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaulteditactions.js"></script>');
+
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaulttags.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-defaultmedia.js"></script>');
+
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-navigations.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-users.js"></script>');
+
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/u-notifier.js"></script>');
+
+
+document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_8/src/u-googleanalytics.js"></script>');
+
+
