a42d276c8285ccccf3454d20c101fad176c885c8
kaestel <kaestel@e-types.com>
2015-02-21 18:01:22 +0000

    Fixed id for radiobuttons

--- src/classes/system/html.core.class.php
+++ src/classes/system/html.core.class.php
@@ -209,7 +209,7 @@ class HTMLCore {
 				foreach($options as $radio_value => $radio_label) {
 
 					$radio_for = "input_".preg_replace("/\[\]/", "", $name."_".$radio_value);
-					$att_radio_id = $this->attribute("id", $for);
+					$att_radio_id = $this->attribute("id", $radio_for);
 					$att_radio_value = $this->attribute("value", $radio_value);
 					$att_radio_checked = $this->attribute("checked", ($value == $radio_value ? "checked" : ""));
 
