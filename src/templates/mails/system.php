71a99ad8152e7ec126230eef6d09f10261c1df70
kaestel <kaestel@e-types.com>
2015-02-23 17:51:56 +0000

    Added more debug output in system mail template

--- src/templates/mails/system.php
+++ src/templates/mails/system.php
@@ -3,4 +3,16 @@
 $message .= "\n\n".$_SERVER["HTTP_HOST"]."\n".$_SERVER["REQUEST_URI"]."\n";
 $message .= getenv("HTTP_X_FORWARDED_FOR") ? getenv("HTTP_X_FORWARDED_FOR") : getenv("REMOTE_ADDR");
 
+$message .= "\n\nMessages:\n";
+$message .= print_r(message()->getMessages(), true);
+
+$message .= "\n\n_POST\n";
+$message .= print_r($_POST, true);
+
+$message .= "\n\n_GET\n";
+$message .= print_r($_GET, true);
+
+$message .= "\n\n_SERVER\n";
+$message .= print_r($_SERVER, true);
+
 ?>
