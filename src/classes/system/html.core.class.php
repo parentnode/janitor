9ade58ab33b8f81d0db03cb86b48d56dc2b62f91
kaestel <kaestel@e-types.com>
2015-03-10 16:06:57 +0000

    Specific function for printing navigation links

--- src/classes/system/html.core.class.php
+++ src/classes/system/html.core.class.php
@@ -887,6 +887,35 @@ class HTMLCore {
 		return $_;
 	}
 
+	/**
+	* Create a simple A HREF link with access validation
+	*
+	* @param $value String text value for A-tag
+	* @param $action String HREF value to be validated
+	* @param $_options Array of optional settings
+	*/
+	function navigationLink($node) {
+
+		global $page;
+		if(!$page->validatePath($node["link"])) {
+			if($node["fallback"] && $page->validatePath($node["fallback"])) {
+				$node["link"] = $node["fallback"];
+			} else {
+				return "";
+			}
+		}
+
+
+		$_ = "";
+
+		$att_class = $this->attribute("class", $node["classname"]);
+		$att_target = $this->attribute("target", $node["target"]);
+
+		$_ .= '<li'.$att_class.'><a href="'.$node["link"].'"'.$att_target.'>'.$node["name"].'</a></li>';
+
+		return $_;
+	}
+
 
 	/**
 	* Start a form tag
