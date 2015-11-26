1fc8f7402f1737329f369f78d17498028a1eff44
kaestel <martin@think.dk>
2015-11-26 11:11:32 +0100

    Moved u-notifier to Manipulator

--- src/www/js/lib/seg_desktop_include.js
+++ src/www/js/lib/seg_desktop_include.js
@@ -1,6 +1,7 @@
 document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_9/full/seg_desktop.js"></script>');
 
 
+document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-notifier.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-navigation.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-history.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-form-htmleditor.js"></script>');
@@ -25,7 +26,7 @@ document.write('<script type="text/javascript" src="/janitor/admin/js/lib/deskto
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-users.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-profile.js"></script>');
 
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/u-notifier.js"></script>');
+//document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/u-notifier.js"></script>');
 
 // document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
 // document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_9/src/u-googleanalytics.js"></script>');
