350f7ad5f773583ee5ab168bebec68622bee56ee
kaestel <martin@think.dk>
2015-12-24 14:29:25 +0100

    Updated text

--- src/www/js/lib/i-unsupported.js
+++ src/www/js/lib/i-unsupported.js
@@ -1,6 +1,6 @@
 function unsupported() {
 //	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333">Your browser is NOT supported. It is more outdated than steam-engines, typewriters and VHS tapes, so stop acting surprised.</h1><p style="text-align: center; margin: 0 10%; font-family: Arial; color: #333333">or<br />(you changed your UserAgent)<br />(we are working on it)<br />(we made an error - <a href="mailto:martin@parentnode.dk">let us know</a>)</p>';
-	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333">Your browser is NOT supported in Janitor yet.</p>';
+	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333, font-size: 16px;">Sorry, your browser is not supported in Janitor yet<br /> - we are working on it!</p>';
 }
 
 window.onload = unsupported;
