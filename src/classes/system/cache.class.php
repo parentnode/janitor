c94d59e9d0ad93935d40d0c1603b6f8496a88005
kaestel <martin@think.dk>
2016-03-08 03:18:29 +0000

    Added unserializer to cache class

--- src/classes/system/cache.class.php
+++ src/classes/system/cache.class.php
@@ -15,11 +15,11 @@ class Cache {
 
 			// set value
 			if($value) {
-				$memc->set($key, json_encode($value));
+				$memc->set(SITE_URL."-".$key, json_encode($value));
 			}
 			// get value
 			else {
-				return json_decode($memc->get($key), true);
+				return json_decode($memc->get(SITE_URL."-".$key), true);
 			}
 
 		}
@@ -33,9 +33,42 @@ class Cache {
 			$memc = new Memcached();
 			$memc->addServer('localhost', 11211);
 
-			$memc->delete($key);
+			$memc->delete(SITE_URL."-".$key);
 
 		}
+	}
+
+	
+	function unserializeSession($data) {
+
+		$vars = preg_split('/([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)\|/', $data, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+		for($i = 0; isset($vars[$i]); $i++) {
+			$results[$vars[$i++]] = unserialize($vars[$i]);
+		}
+
+		foreach($results as $index => $result) {
+
+			$results[$index] = $this->decodeSessionJSON($result);
+			
+		}
+		return $results;
+	}
+	
+	function decodeSessionJSON($dataset) {
+
+		if(is_array($dataset)) {
+			foreach($dataset as $index =>$data) {
+				$dataset[$index] = $this->decodeSessionJSON($data);
+			}
+			return $dataset;
+		}
+		else if(!is_object($dataset)) {
+			return json_decode($dataset, true);
+		}
+		else {
+			return $dataset;
+		}
+
 	}
 }
 
