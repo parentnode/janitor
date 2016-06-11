adb1092f6d86599d51237b438043bd307b3523f9
kaestel <martin@think.dk>
2016-06-11 23:43:23 +0200

    Added signup templates

---
+++ src/templates/signup/confirmed.php
@@ -0,0 +1,71 @@
+<?php
+global $action;
+global $model;
+
+$IC = new Items();
+$page_item = $IC->getItem(array("tags" => "page:signup-confirmed", "extend" => array("user" => true, "mediae" => true)));
+if($page_item) {
+	$this->sharingMetaData($page_item);
+}
+
+$type = $action[1];
+$username = $action[2];
+
+?>
+<div class="scene signup i:scene">
+
+<? if($page_item && $page_item["status"]): 
+	$media = $IC->sliceMedia($page_item); ?>
+	<div class="article i:article id:<?= $page_item["item_id"] ?>" itemscope itemtype="http://schema.org/Article">
+
+		<? if($media): ?>
+		<div class="image item_id:<?= $page_item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
+		<? endif; ?>
+
+		<h1 itemprop="headline"><?= $page_item["name"] ?></h1>
+
+		<? if($page_item["subheader"]): ?>
+		<h2 itemprop="alternativeHeadline"><?= $page_item["subheader"] ?></h2>
+		<? endif; ?>
+
+		<ul class="info">
+			<li class="published_at" itemprop="datePublished" content="<?= date("Y-m-d", strtotime($page_item["published_at"])) ?>"><?= date("Y-m-d, H:i", strtotime($page_item["published_at"])) ?></li>
+			<li class="modified_at" itemprop="dateModified" content="<?= date("Y-m-d", strtotime($page_item["modified_at"])) ?>"><?= date("Y-m-d, H:i", strtotime($page_item["published_at"])) ?></li>
+			<li class="author" itemprop="author"><?= $page_item["user_nickname"] ?></li>
+			<li class="main_entity" itemprop="mainEntityOfPage"><?= SITE_URL."/signup/confirm" ?></li>
+			<li class="publisher" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+				<ul class="publisher_info">
+					<li class="name" itemprop="name">think.dk</li>
+					<li class="logo" itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+						<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/img/logo-large.png"></span>
+						<span class="image_width" itemprop="width" content="720"></span>
+						<span class="image_height" itemprop="height" content="405"></span>
+					</li>
+				</ul>
+			</li>
+			<li class="image_info" itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+			<? if($media): ?>
+				<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/images/<?= $page_item["item_id"] ?>/<?= $media["variant"] ?>/720x.<?= $media["format"] ?>"></span>
+				<span class="image_width" itemprop="width" content="720"></span>
+				<span class="image_height" itemprop="height" content="<?= floor(720 / ($media["width"] / $media["height"])) ?>"></span>
+			<? else: ?>
+				<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/img/logo-large.png"></span>
+				<span class="image_width" itemprop="width" content="720"></span>
+				<span class="image_height" itemprop="height" content="405"></span>
+			<? endif; ?>
+			</li>
+		</ul>
+
+		<? if($page_item["html"]): ?>
+		<div class="articlebody" itemprop="articleBody">
+			<?= preg_replace("/{type}/", $type, preg_replace("/{username}/", $username, $page_item["html"])) ?>
+		</div>
+		<? endif; ?>
+	</div>
+<? else:?>
+	<h1>Thank you!</h1>
+	<p>Your <?= $type ?>: <?= $username ?>, has been confirmed.</p>
+<? endif; ?>
+
+
+</div>
