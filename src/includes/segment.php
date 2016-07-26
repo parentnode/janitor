a0e5a8f173f5a9f164d6ee97b39a83b841777e44
kaestel <martin@think.dk>
2016-07-26 12:29:29 +0200

    Include segments.core conversion in simple segment script

--- src/includes/segment.php
+++ src/includes/segment.php
@@ -1,21 +1,43 @@
 <?php
 
 	session_start();
+	// segment parameter sent
 	if(isset($_GET["segment"])) {
-		$_SESSION["segment"] = $_GET["segment"];
-	}
-	if(!isset($_SESSION["segment"])) {
 
-		$segment = @file_get_contents("http://devices-v3.dearapi.com/text?ua=".urlencode($_SERVER["HTTP_USER_AGENT"])."&site=".urlencode($_SERVER["HTTP_HOST"])."&file=".urlencode($_SERVER["SCRIPT_NAME"]));
-//		$device_id = file_get_contents("http://devices.local/xml?ua=".urlencode($_SERVER["HTTP_USER_AGENT"])."&site=".urlencode($_SERVER["HTTP_HOST"])."&file=".urlencode($_SERVER["SCRIPT_NAME"]));
-		if($segment) {
-			$_SESSION["segment"] = $segment;
+		$segment = $_GET["segment"];
+
+	}
+	// no session value, look up segment
+	else if(!isset($_SESSION["segment"])) {
+
+		$segment = file_get_contents("http://detector-v3.dearapi.com/text?ua=".urlencode($_SERVER["HTTP_USER_AGENT"])."&site=".urlencode($_SERVER["HTTP_HOST"])."&file=".urlencode($_SERVER["SCRIPT_NAME"]));
+
+		if(!$segment) {
+			$segment = "desktop";
+		}
+	}
+
+	// update segment session value
+	if($segment) {
+
+		// DOES PROJECT HAVE FRAMEWORK SEGMENTS
+		if(isset($_SERVER["FRAMEWORK_PATH"]) && file_exists($_SERVER["FRAMEWORK_PATH"]."/config/segments.core.php")) {
+			@include_once($_SERVER["FRAMEWORK_PATH"]."/config/segments.core.php");
+
+			// DOES PROJECT HAVE CUSTOM SEGMENTS
+			if(isset($_SERVER["LOCAL_PATH"]) && file_exists($_SERVER["LOCAL_PATH"]."/config/segments.core.php")) {
+				@include_once($_SERVER["LOCAL_PATH"]."/config/segments.php");
+			}
+		}
+
+		if(isset($segments_config) && isset($segments_config["www"][$segment])) {
+			$_SESSION["segment"] = $segments_config["www"][$segment];
 		}
 		else {
-			$_SESSION["segment"] = "desktop";
+			$_SESSION["segment"] = $segment;
 		}
+		
 	}
-
 
 	// debug helper
 	if(isset($_GET["dev"])) {
