b6292f3e3f37a88449869218f4d9e8d0982bf2ff
kaestel <martin@think.dk>
2016-11-09 22:06:43 +0000

    Added new options to example

--- src/config/config.example.php
+++ src/config/config.example.php
@@ -20,6 +20,8 @@ define("SITE_EMAIL", "mail@domain.com");
 * Optional constants
 */
 define("DEFAULT_PAGE_DESCRIPTION", "");
+define("DEFAULT_PAGE_IMAGE", "");
+
 define("DEFAULT_LANGUAGE_ISO", "EN");
 define("DEFAULT_COUNTRY_ISO", "DK");
 define("DEFAULT_CURRENCY_ISO", "DKK");
@@ -29,7 +31,14 @@ define("DEFAULT_PAGE_IMAGE", "/touchicon.png");
 
 // ENABLE ITEMS MODEL
 define("SITE_ITEMS", false);
-define("SITE_SIGNUP", false);
+define("SITE_SIGNUP", "/path_to_signup_page");
+
+define("SITE_SUBSCRIPTIONS", true);
+define("SITE_MEMBERS", true);
+
+// Enable shop model
+define("SITE_SHOP", true);
+define("SHOP_ORDER_NOTIFIES", "email@domain.dk,anotheremail@domain.dk");
 
 // Enable notifications (send collection email after N notifications)
 define("SITE_COLLECT_NOTIFICATIONS", 50);
