1726f7e85db4a47f392fd8daa2e7929432db561b
kaestel <martin@think.dk>
2017-01-11 00:20:56 +0100

    New PDF Class

---
+++ src/classes/system/pdf.class.php
@@ -0,0 +1,37 @@
+<?php
+
+// BETA PDF CLASS
+// CURRENTLY ONLY INCLUDED TO TEST wkhtmlto module
+class PDF {
+
+
+	function create($input_file, $output_file, $options = false) {
+
+		$fs = new FileSystem();
+
+		$output_format = "A4";
+
+		if($options !== false) {
+			foreach($options as $option => $value) {
+				switch($option) {
+					case "format"            : $output_format      = $value; break;
+				}
+			}
+		}
+
+		$wkhtmlto_path = wkhtmltoPath();
+
+		$command = "$wkhtmlto_path -s $output_format $input_file $output_file";
+		// --javascript-delay 5000 --no-stop-slow-scripts --enable-javascript --debug-javascript
+		// print "<p>"+$command."</p>";
+
+		// Generate the image
+		$output = shell_exec($command." 2>&1");
+		// print "output:" . $output;
+
+		return $output_file;
+	}
+
+}
+
+?>
