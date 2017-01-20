ffb9f860db6bcc99f5e10b50b2ce4c6cb74756b9
Martin Kæstel Nielsen <martin@think.dk>
2017-01-20 17:04:32 +0100

    Changed upgrade type

--- src/templates/upgrade/full.php
+++ src/templates/upgrade/full.php
@@ -4,11 +4,11 @@ global $model;
 	
 ?>
 <div class="scene i:scene">
-	<h1>Upgrading database</h1>
+	<h1>Full upgrade</h1>
 
 	<h3>Upgrade process:</h3>
 	<ul class="tasks">
-		<? $model->upgradeDatabase(); ?>
+		<? $model->fullUpgrade(); ?>
 	</ul>
 
 </div>
