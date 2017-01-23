18c635a221c151c5c00c7b1ea3e6bdb81afa5101
Martin Kæstel Nielsen <martin@think.dk>
2017-01-23 09:52:10 +0100

    debug

--- src/templates/janitor/system/cache/list.php
+++ src/templates/janitor/system/cache/list.php
@@ -7,7 +7,7 @@ if(class_exists("Memcached")) {
 
 	$memc = new Memcached();
 	$con = $memc->addServer('localhost', 11211);
-	print "con:".$con.";"
+	print "con:".$con.";";
 	print_r($memc->getStats());
 
 	$entries = array();
