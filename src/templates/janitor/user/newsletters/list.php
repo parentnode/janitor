1f88fd445073e2ef4298734cea644deecee37301
Martin Kæstel Nielsen <martin@think.dk>
2017-03-30 04:41:20 +0200

    Filtered inactive users

--- src/templates/janitor/user/newsletters/list.php
+++ src/templates/janitor/user/newsletters/list.php
@@ -2,7 +2,6 @@
 global $action;
 global $model;
 $IC = new Items();
-$SC = new Shop();
 
 $newsletters = $this->newsletters();
 //$memberships = $IC->getItems(array("itemtype" => "membership", "extend" => true));
@@ -30,6 +29,18 @@ if($newsletter_id) {
 }
 
 $subscribers = $model->getNewsletters($options);
+$active_subscribers = [];
+if($subscribers) {
+	
+	foreach($subscribers as $subscriber) {
+		$user = $model->getUsers(["user_id" => $subscriber["user_id"]]);
+		if($user["status"] > 0) {
+			$user["email"] = $model->getUsernames(["user_id" => $subscriber["user_id"], "type" => "email"]);
+			array_push($active_subscribers, $user);
+		}
+	}
+}
+
 //print_r($subscribers);
 ?>
 <div class="scene i:scene defaultList newsletterList">
@@ -45,25 +56,21 @@ $subscribers = $model->getNewsletters($options);
 <?	if($newsletters): ?>
 	<ul class="tabs">
 <?		foreach($newsletters as $newsletter): ?>
-		<?= $HTML->link($newsletter["name"], "/janitor/admin/user/newsletters/list/".$newsletter["id"], array("wrapper" => "li.".($newsletter["id"] == $newsletter_id ? "selected" : ""))) ?>
+		<?= $HTML->link($newsletter["name"].($newsletter["id"] == $newsletter_id ? " (".count($active_subscribers).")" : ""), "/janitor/admin/user/newsletters/list/".$newsletter["id"], array("wrapper" => "li.".($newsletter["id"] == $newsletter_id ? "selected" : ""))) ?>
 <?		endforeach; ?>
 		<?= $HTML->link("All", "/janitor/admin/user/newsletters/list/0", array("wrapper" => "li.".($options === false ? "selected" : ""))) ?>
 	</ul>
 <?	endif; ?>
 
-
 	<div class="all_items i:defaultList filters">
-<?		if($subscribers): ?>
+<?		if($active_subscribers): ?>
 		<ul class="items">
-<?			foreach($subscribers as $subscriber):
-				$email = $model->getUsernames(["user_id" => $subscriber["user_id"], "type" => "email"]);
-				$user = $model->getUsers(["user_id" => $subscriber["user_id"]]);
-?>
+<?			foreach($active_subscribers as $subscriber): ?>
 			<li class="item user_id_id:<?= $subscriber["user_id"] ?>">
-				<h3><?= $email ?></h3>
+				<h3><?= $subscriber["email"] ?></h3>
 				<dl class="info">
 					<dt class="nickname">Nickname</dt>
-					<dd class="nickname"><?= $user["nickname"] ?></dd>
+					<dd class="nickname"><?= $subscriber["nickname"] ?></dd>
 				</dl>
 			 </li>
 <?			endforeach; ?>
