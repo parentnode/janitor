e353520d99a713c346823cc94c4a0994b3a02e29
Martin Kæstel Nielsen <martin@think.dk>
2017-05-04 17:14:11 +0200

    whitespace

--- src/classes/system/session.class.php
+++ src/classes/system/session.class.php
@@ -63,7 +63,7 @@ class Session {
 		else {
 //			session_unset();
 			unset($_SESSION);
-			
+
 			if(ini_get("session.use_cookies")) {
 				$params = session_get_cookie_params();
 				setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
