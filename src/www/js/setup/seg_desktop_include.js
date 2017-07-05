3b9302984f0f99f1774254408c4a6cf439b3257a
Martin Kæstel Nielsen <martin@think.dk>
2017-07-05 13:31:00 +0200

    Made setup js include be independent of dev-modules

--- src/www/js/setup/seg_desktop_include.js
+++ src/www/js/setup/seg_desktop_include.js
@@ -1,4 +1,4 @@
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/seg_desktop_include.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/js/seg_desktop.js"></script>');
 
 //document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-page.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/i-progress.js"></script>');
