e3c4b8b3cdfae90d92f08698fbf3306688062053
Martin Kæstel Nielsen <martin@think.dk>
2017-07-23 13:03:00 +0200

    constraints update

--- src/config/db/user_log_payment_reminders.sql
+++ src/config/db/user_log_payment_reminders.sql
@@ -8,6 +8,6 @@ CREATE TABLE `SITE_DB`.`user_log_payment_reminders` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `order_id` (`order_id`),
-  CONSTRAINT `user_log_payment_reminders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `SITE_DB`.`users` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `user_log_payment_reminders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `SITE_DB`.`users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT `user_log_payment_reminders_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `SITE_DB`.`shop_orders` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
