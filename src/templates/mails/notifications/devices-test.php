10ced2cd42e3582e484160229fe7b8b07a0428e4
Martin Kæstel Nielsen <martin@think.dk>
2018-01-20 22:46:35 +0100

    updated emails

--- src/templates/mails/notifications/devices-test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$message = preg_replace("/\; REFERRER\:/", ";\nREFERRER:", $message);
-$message = preg_replace("/\; USERAGENT\:/", ";\nUSERAGENT:", $message);
-$message = preg_replace("/\; IDENTIFIED\:/", ";\nIDENTIFIED:", $message);
-
-?>
---
+++ src/templates/mails/system.html.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+	<title>System template from {SITE_URL}</title>
+</head>
+<body>
+
+
+<h3>{message} test</h3>
+
+
+<h2>------ ADDITIONAL SERVER INFO ------</h2>
+
+
+<dl>
+	<dt>Origin:</dt>
+	<dd><?= ($_SERVER["HTTPS"] ? "https" : "http") ?>://<?= $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"] ?></dd>
+
+	<dt>From IP:</dt>
+	<dd><?= (getenv("HTTP_X_FORWARDED_FOR") ? getenv("HTTP_X_FORWARDED_FOR") : getenv("REMOTE_ADDR")) ?></dd>
+
+	<dt>Referer:</dt>
+	<dd><?= $_SERVER["HTTP_REFERER"] ?></dd>
+
+	<dt>UserAgent:</dt>
+	<dd><?= $_SERVER["HTTP_USER_AGENT"] ?></dd>
+</dl>
+
+
+<h3>Segment:</h3>
+<code style="white-space: pre-wrap;"><?= print_r(session()->value("segment"), true) ?></code>
+
+<h3>Messages:</h3>
+<code style="white-space: pre-wrap;"><?= print_r(message()->getMessages(), true) ?></code>
+
+<h3>_SESSION:</h3>
+<code style="white-space: pre-wrap;"><?= print_r($_SESSION, true) ?></code>
+
+<h3>_POST:</h3>
+<code style="white-space: pre-wrap;"><?= print_r($_POST, true) ?></code>
+
+<h3>_GET:</h3>
+<code style="white-space: pre-wrap;"><?= print_r($_GET, true) ?></code>
+
+<h3>_FILES:</h3>
+<code style="white-space: pre-wrap;"><?= print_r($_FILES, true) ?></code>
+
+<h3>_SERVER:</h3>
+<code style="white-space: pre-wrap;"><?= print_r($_SERVER, true) ?></code>
+
+</body>
+</html>
