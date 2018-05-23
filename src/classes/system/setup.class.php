6aa80a27ed2ca2091d9abab4045ea77865d3962a
Martin Kæstel Nielsen <martin@think.dk>
2018-05-23 20:47:27 +0200

    Win

--- src/classes/system/setup.class.php
+++ src/classes/system/setup.class.php
@@ -1519,7 +1519,7 @@ class Setup extends Itemtype {
 
 					if($this->get("system", "os") == "win") {
 						// Extract
-						$output = shell_exec("C:/Program Files/7-Zip/7z.exe e ".PROJECT_PATH."/theme.tar.gz");
+						$output = shell_exec('"C:/Program Files/7-Zip/7z.exe" e "'.PROJECT_PATH.'/theme.tar.gz"');
 					}
 					else {
 						// Extract
