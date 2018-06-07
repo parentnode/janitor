4cafb62d7e645e247f05e0f0907e37bc6a8eedab
Martin Kæstel Nielsen <martin@think.dk>
2018-06-07 15:03:39 +0200

    Revised: upgrade should never take 30 secs, time limit restored

--- src/www/setup.php
+++ src/www/setup.php
@@ -7,9 +7,6 @@ if(isset($read_access) && $read_access) {
 
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init-setup.php");
 
-// Upgrade and setup may take longer than 30 secs.
-set_time_limit(0);
-
 
 $action = $page->actions();
 
