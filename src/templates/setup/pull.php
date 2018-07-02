aefc03e83c59707c44600ad481bb2e5259386345
Martin Kæstel Nielsen <martin@think.dk>
2018-07-02 15:32:23 +0200

    Require credential setup to allow pull (to avoid password exposure)

--- src/templates/setup/pull.php
+++ src/templates/setup/pull.php
@@ -7,12 +7,21 @@ $project_path = realpath(LOCAL_PATH."/..");
 // Get git origin
 $remote_origin = trim(shell_exec("cd '$project_path' && git config --get remote.origin.url"));
 
+
 // Test origins
 // $remote_origin = "https://kaestel:pass@github.com/kaestel/casepoint_dk";
 // $remote_origin = "https://kaestel@github.com/kaestel/casepoint_dk";
 // $remote_origin = "https://github.com/kaestel/casepoint_dk";
 
 $remote_origin = preg_replace("/(http[s]?):\/\/(([^:]+)[:]?([^@]+)@)?/", "$1://", $remote_origin);
+
+
+// Check that git has been set up with project credentials file
+$credential_helper = trim(shell_exec("cd '$project_path' && git config --get credential.helper"));
+$credential_helper_ready = false;
+if(strpos("store --file $project_path/.git_credentials", $credential_helper) === 0) {
+	$credential_helper_ready = true;
+}
 
 ?>
 <div class="scene pull i:pull">
@@ -22,27 +31,36 @@ $remote_origin = preg_replace("/(http[s]?):\/\/(([^:]+)[:]?([^@]+)@)?/", "$1://"
 
 <? if($model->get("system", "os") == "unix"): ?>
 
-	<p>Your are about to pull the latest source code from:<br /><?= $remote_origin ?>.</p>
+	<? if($credential_helper_ready): ?>
 
-	<p class="note">
-		If your repository is private, you need to enter your git username and password to 
-		be able to pull, otherwise you can leave the fields empty.
-	</p>
+		<p>Your are about to pull the latest source code from:<br /><?= $remote_origin ?>.</p>
 
-	<?= $model->formStart("/janitor/admin/setup/pull", array("class" => "pull labelstyle:inject")) ?>
-		<?= $model->input("pull", array("type" => "hidden", "value" => "ok")); ?>
+		<p class="note">
+			If your repository is private, you need to enter your git username and password to 
+			be able to pull, otherwise you can leave the fields empty.
+		</p>
 
-		<fieldset>
-			<?= $model->input("git_username", array("label" => "Git username", "type" => "string", "hint_message" => "Enter your git username", "error_message" => "Not a valid username")); ?>
-			<?= $model->input("git_password", array("label" => "Git password", "type" => "password", "hint_message" => "Enter your git password", "error_message" => "Not a valid password")); ?>
-		</fieldset>
+		<?= $model->formStart("/janitor/admin/setup/pull", array("class" => "pull labelstyle:inject")) ?>
+			<?= $model->input("pull", array("type" => "hidden", "value" => "ok")); ?>
 
-		<ul class="actions">
-			<?= $model->submit("Pull now", array("class" => "primary", "wrapper" => "li.pull")) ?>
-		</ul>
-	<?= $model->formEnd() ?>
+			<fieldset>
+				<?= $model->input("git_username", array("label" => "Git username", "type" => "string", "hint_message" => "Enter your git username", "error_message" => "Not a valid username")); ?>
+				<?= $model->input("git_password", array("label" => "Git password", "type" => "password", "hint_message" => "Enter your git password", "error_message" => "Not a valid password")); ?>
+			</fieldset>
 
-	<p class="pull_result"></p>
+			<ul class="actions">
+				<?= $model->submit("Pull now", array("class" => "primary", "wrapper" => "li.pull")) ?>
+			</ul>
+		<?= $model->formEnd() ?>
+
+		<p class="pull_result"></p>
+
+	<? else: ?>
+
+		<p>Git has not been set up for pulling through Janitor yet. Contact your server administrator to set it up.</p>
+
+	<? endif; ?>
+
 
 <? else: ?>
 
