922992659ee15a2dc4dc57997b72ba19cad26dd2
kelete <kelete@yahoo.com>
2019-03-15 13:41:15 +0100

    Revert of changes in file
    Revert from commit ff6e01f  changes. only on file class.phpmailer.php

--- src/includes/mailer/PHPMailer-5.2.26/class.phpmailer.php
+++ src/includes/mailer/PHPMailer-5.2.26/class.phpmailer.php
@@ -1142,7 +1142,7 @@ class PHPMailer
                  * @link http://www.whatwg.org/specs/web-apps/current-work/#e-mail-state-(type=email)
                  */
                 return (boolean)preg_match(
-                    '/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-\+]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}' .
+                    '/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}' .
                     '[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/sD',
                     $address
                 );
