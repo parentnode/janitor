ffc303836fa3be6348d713710909c4b20edb71a1
Martin Kæstel Nielsen <martin@think.dk>
2019-10-03 20:59:14 +0200

    Remove comment

--- src/config/init.php
+++ src/config/init.php
@@ -84,9 +84,6 @@ if(defined("SITE_ITEMS") && SITE_ITEMS) {
 	include_once("classes/users/user.core.class.php");
 	include_once("classes/users/user.class.php");
 
-	// now only included for user controller
-	// include_once("classes/users/superuser.class.php");
-
 	//include_once("classes/system/performance.class.php");
 
 
