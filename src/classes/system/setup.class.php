3f3736e2033708681fe9bf23408d7f07206d9fd8
Søren Lyhne <lyhnesoren@gmail.com>
2020-01-07 11:22:53 +0100

    add UT_PRICE_TYPES to setup

--- src/classes/system/setup.class.php
+++ src/classes/system/setup.class.php
@@ -1869,6 +1869,7 @@ class Setup extends Itemtype {
 			$query->checkDbExistence(UT_CURRENCIES);
 			$query->checkDbExistence(UT_COUNTRIES);
 			$query->checkDbExistence(UT_VATRATES);
+			$query->checkDbExistence(UT_PRICE_TYPES);
 			$query->checkDbExistence(UT_PAYMENT_METHODS);
 			$query->checkDbExistence($UC->db_user_groups);
 			$query->checkDbExistence($UC->db);
