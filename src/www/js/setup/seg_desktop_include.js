53e7dd095e1ae09b64b3cdadd6f961db22403098
Martin Kæstel Nielsen <martin@think.dk>
2020-03-19 16:35:53 +0100

    Update manipulator include for setup

--- src/www/js/setup/seg_desktop_include.js
+++ src/www/js/setup/seg_desktop_include.js
@@ -2,11 +2,11 @@ document.write('<script type="text/javascript" src="/janitor/admin/js/manipulato
 document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_9_3-janitor/src/beta-u-notifier.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/manipulator/v0_9_3-janitor/src/beta-u-form-onebuttonform.js"></script>');
 
-document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/i-page.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/lib/desktop/m-page.js"></script>');
 document.write('<script type="text/javascript" src="/janitor/admin/js/lib/u-basics.js"></script>');
 
-document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/i-progress.js"></script>');
-document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/i-upgrade.js"></script>');
-document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/i-pull.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/m-progress.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/m-upgrade.js"></script>');
+document.write('<script type="text/javascript" src="/janitor/admin/js/setup/desktop/m-pull.js"></script>');
 
 
