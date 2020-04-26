94a8ca24068008f90c0c9b7d9a55b88a9cc4554b
Martin Kæstel Nielsen <martin@think.dk>
2020-04-26 10:37:11 +0200

    PHP 7.4 update

--- src/includes/functions.inc.php
+++ src/includes/functions.inc.php
@@ -33,13 +33,12 @@ function debug($vars, $output = "print") {
 /**
 * used by zip
 */
-
 function utf8Encode($string) {
 	$new_string = '';	
 	for($i = 0; $i < strlen($string); $i++){ 
 		//print "$i: ".$string{$i}.": ".mb_detect_encoding($string{$i});
-		if(mb_detect_encoding($string{$i})) {
-			$new_string .= $string{$i};
+		if(mb_detect_encoding($string[$i])) {
+			$new_string .= $string[$i];
 		}
 	}
 	$encoding = mb_detect_encoding($new_string);
@@ -265,7 +264,7 @@ function randomKey($length=false) {
 	$length = $length ? $length : 8;
 	$key = '';
 	for($i = 0; $i < $length; $i++) {
-		$key .= $pattern{rand(0,35)};
+		$key .= $pattern[rand(0,35)];
 	}
 	return $key;
 }
@@ -513,19 +512,19 @@ function superNormalize($string) {
 
 
 function _uniord($c) {
-    if (ord($c{0}) >=0 && ord($c{0}) <= 127)
-        return ord($c{0});
-    if (ord($c{0}) >= 192 && ord($c{0}) <= 223)
-        return (ord($c{0})-192)*64 + (ord($c{1})-128);
-    if (ord($c{0}) >= 224 && ord($c{0}) <= 239)
-        return (ord($c{0})-224)*4096 + (ord($c{1})-128)*64 + (ord($c{2})-128);
-    if (ord($c{0}) >= 240 && ord($c{0}) <= 247)
-        return (ord($c{0})-240)*262144 + (ord($c{1})-128)*4096 + (ord($c{2})-128)*64 + (ord($c{3})-128);
-    if (ord($c{0}) >= 248 && ord($c{0}) <= 251)
-        return (ord($c{0})-248)*16777216 + (ord($c{1})-128)*262144 + (ord($c{2})-128)*4096 + (ord($c{3})-128)*64 + (ord($c{4})-128);
-    if (ord($c{0}) >= 252 && ord($c{0}) <= 253)
-        return (ord($c{0})-252)*1073741824 + (ord($c{1})-128)*16777216 + (ord($c{2})-128)*262144 + (ord($c{3})-128)*4096 + (ord($c{4})-128)*64 + (ord($c{5})-128);
-    if (ord($c{0}) >= 254 && ord($c{0}) <= 255)    //  error
+    if (ord($c[0]) >=0 && ord($c[0]) <= 127)
+        return ord($c[0]);
+    if (ord($c[0]) >= 192 && ord($c[0]) <= 223)
+        return (ord($c[0])-192)*64 + (ord($c[1])-128);
+    if (ord($c[0]) >= 224 && ord($c[0]) <= 239)
+        return (ord($c[0])-224)*4096 + (ord($c[1])-128)*64 + (ord($c[2])-128);
+    if (ord($c[0]) >= 240 && ord($c[0]) <= 247)
+        return (ord($c[0])-240)*262144 + (ord($c[1])-128)*4096 + (ord($c[2])-128)*64 + (ord($c[3])-128);
+    if (ord($c[0]) >= 248 && ord($c[0]) <= 251)
+        return (ord($c[0])-248)*16777216 + (ord($c[1])-128)*262144 + (ord($c[2])-128)*4096 + (ord($c[3])-128)*64 + (ord($c[4])-128);
+    if (ord($c[0]) >= 252 && ord($c[0]) <= 253)
+        return (ord($c[0])-252)*1073741824 + (ord($c[1])-128)*16777216 + (ord($c[2])-128)*262144 + (ord($c[3])-128)*4096 + (ord($c[4])-128)*64 + (ord($c[5])-128);
+    if (ord($c[0]) >= 254 && ord($c[0]) <= 255)    //  error
         return FALSE;
     return 0;
 }
