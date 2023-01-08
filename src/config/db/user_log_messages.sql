befe21991a418cf47365cd061320d8deb8547c43
kaestel <martin@think.dk>
2023-01-08 13:53:19 +0100

    Prevent message log from blocking deletion of messages

--- src/config/db/user_log_messages.sql
+++ src/config/db/user_log_messages.sql
@@ -12,5 +12,5 @@ CREATE TABLE `SITE_DB`.`user_log_messages` (
   KEY `user_id` (`user_id`),
   KEY `item_id` (`item_id`),
   CONSTRAINT `user_log_messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `SITE_DB`.`users` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT `user_log_messages_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `SITE_DB`.`items` (`id`) ON UPDATE CASCADE
+  CONSTRAINT `user_log_messages_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `SITE_DB`.`items` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
