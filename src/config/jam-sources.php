401fb14b56baa8b0cdacb42ce5304d4df883f7a2
kaestel <martin@think.dk>
2025-01-09 09:04:57 +0000

    Updated module naming

--- src/config/jam-sources.php
+++ src/config/jam-sources.php
@@ -1,28 +1,28 @@
 <?php
 	
 $jam_sources = [
-	"instantmessages" => [
+	"instantmessage" => [
 		"name" => "Instant message gateways",
 		"modules" => [
 			"telegram" => [
 				"name" => "Telegram messenger",
 				"info_link" => "https://telegram.org/",
-				"repos" =>	"https://github.com/parentnode/jam-instantmessages-telegram",
+				"repos" =>	"https://github.com/parentnode/jam-instantmessage-telegram",
 			],
 		]
 	],
-	"mailer" => [
-		"name" => "Mail gateways",
+	"email" => [
+		"name" => "Email gateways",
 		"modules" => [
 			"mailgun" => [
 				"name" => "Mailgun",
 				"info_link" => "https://mailgun.com",
-				"repos" => "https://github.com/parentnode/jam-mailer-mailgun",
+				"repos" => "https://github.com/parentnode/jam-email-mailgun",
 			],
-			"phpmailer" => [
-				"name" => "PHPMailer",
+			"smtp" => [
+				"name" => "SMTP with PHPMailer",
 				"info_link" => "https://github.com/PHPMailer/PHPMailer",
-				"repos" => "https://github.com/parentnode/jam-mailer-phpmailer",
+				"repos" => "https://github.com/parentnode/jam-email-smtp",
 			],
 		]
 		
