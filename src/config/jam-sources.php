797e811f78a136f90b9600c9b28378a3f3f3b1da
kaestel <martin@think.dk>
2026-06-12 11:15:49 +0200

    Adjusting jam sources

--- src/config/jam-sources.php
+++ src/config/jam-sources.php
@@ -12,68 +12,90 @@ $jam_sources = [
 				"repos" => "http://jam-item-page-simplest.local",
 				// "repos" => "https://github.com/parentnode/jam-item-page-simplest",
 			],
+			"post" => [
+				"name" => "Post itemtype",
+				"description" => "The simplest content item for posts.",
+				"info_link" => "https://github.com/parentnode/jam-item-post-simplest",
+				"repos" => "http://jam-item-post-simplest.local",
+				// "repos" => "https://github.com/parentnode/jam-item-post-simplest",
+			],
+			"event" => [
+				"name" => "Event itemtype",
+				"description" => "The simplest content item for events.",
+				"info_link" => "https://github.com/parentnode/jam-item-event-simplest",
+				"repos" => "http://jam-item-event-simplest.local",
+				// "repos" => "https://github.com/parentnode/jam-item-event-simplest",
+			],
 			"service" => [
 				"name" => "Service itemtype",
 				"description" => "The simplest content item for describing the services offered.",
 				"info_link" => "https://github.com/parentnode/jam-item-service-simplest",
-				"repos" => "https://github.com/parentnode/jam-item-service-simplest",
+				"repos" => "http://jam-item-service-simplest.local",
+				// "repos" => "https://github.com/parentnode/jam-item-service-simplest",
 			],
-		] 
-	],
-	"instantmessage" => [
-		"name" => "Instant message gateways",
-		"description" => "Modules to send instant messages via popular messaging platforms.",
-		"modules" => [
-			"telegram" => [
-				"name" => "Telegram messenger",
-				"description" => "Telegram instant message gateway integration",
-				"info_link" => "https://telegram.org/",
-				"repos" =>	"https://github.com/parentnode/jam-instantmessage-telegram",
+			"product" => [
+				"name" => "Product itemtype",
+				"description" => "The simplest content item for products.",
+				"info_link" => "https://github.com/parentnode/jam-item-product-simplest",
+				"repos" => "http://jam-item-product-simplest.local",
+				// "repos" => "https://github.com/parentnode/jam-item-product-simplest",
 			],
 		]
 	],
-	"email" => [
-		"name" => "Email gateways",
-		"description" => "Modules to send email notifications and messages.",
-		"modules" => [
-			"mailgun" => [
-				"name" => "Mailgun",
-				"description" => "Mailgun email gateway integration",
-				"info_link" => "https://mailgun.com",
-				"repos" => "https://github.com/parentnode/jam-email-mailgun",
-			],
-			"smtp" => [
-				"name" => "SMTP with PHPMailer",
-				"description" => "SMTP email gateway using PHPMailer library",
-				"info_link" => "https://github.com/PHPMailer/PHPMailer",
-				"repos" => "https://github.com/parentnode/jam-email-smtp",
-			],
-		]
-		
-	],
-	"sms" => [
-		"name" => "SMS message gateways",
-		"description" => "Modules to send SMS messages via popular SMS platforms.",
-		"modules" => [
-			"twilio" => [
-				"name" => "Twilio",
-				"description" => "Twilio SMS gateway integration",
-				"info_link" => "https://twilio.com",
-				"repos" => "https://github.com/parentnode/jam-sms-twilio",
-			],
-		] 
-	],
-	"fraudprotection" => [
-		"name" => "Fraud protections systems",
-		"description" => "Modules to protect forms and other user inputs from spam and abuse.",
-		"modules" => [
-			"recaptcha" => [
-				"name" => "reCAPTCHA",
-				"description" => "Google reCAPTCHA v3 protection system",
-				"info_link" => "https://www.google.com/recaptcha/intro/v3",
-				"repos" => "https://github.com/parentnode/jam-fraudprotection-recaptcha",
-			],
-		] 
-	],
+	// "instantmessage" => [
+	// 	"name" => "Instant message gateways",
+	// 	"description" => "Modules to send instant messages via popular messaging platforms.",
+	// 	"modules" => [
+	// 		"telegram" => [
+	// 			"name" => "Telegram messenger",
+	// 			"description" => "Telegram instant message gateway integration",
+	// 			"info_link" => "https://telegram.org/",
+	// 			"repos" =>	"https://github.com/parentnode/jam-instantmessage-telegram",
+	// 		],
+	// 	]
+	// ],
+	// "email" => [
+	// 	"name" => "Email gateways",
+	// 	"description" => "Modules to send email notifications and messages.",
+	// 	"modules" => [
+	// 		"mailgun" => [
+	// 			"name" => "Mailgun",
+	// 			"description" => "Mailgun email gateway integration",
+	// 			"info_link" => "https://mailgun.com",
+	// 			"repos" => "https://github.com/parentnode/jam-email-mailgun",
+	// 		],
+	// 		"smtp" => [
+	// 			"name" => "SMTP with PHPMailer",
+	// 			"description" => "SMTP email gateway using PHPMailer library",
+	// 			"info_link" => "https://github.com/PHPMailer/PHPMailer",
+	// 			"repos" => "https://github.com/parentnode/jam-email-smtp",
+	// 		],
+	// 	]
+	//
+	// ],
+	// "sms" => [
+	// 	"name" => "SMS message gateways",
+	// 	"description" => "Modules to send SMS messages via popular SMS platforms.",
+	// 	"modules" => [
+	// 		"twilio" => [
+	// 			"name" => "Twilio",
+	// 			"description" => "Twilio SMS gateway integration",
+	// 			"info_link" => "https://twilio.com",
+	// 			"repos" => "https://github.com/parentnode/jam-sms-twilio",
+	// 		],
+	// 	]
+	// ],
+	// "fraudprotection" => [
+	// 	"name" => "Fraud protections systems",
+	// 	"description" => "Modules to protect forms and other user inputs from spam and abuse.",
+	// 	"modules" => [
+	// 		"recaptcha" => [
+	// 			"name" => "reCAPTCHA",
+	// 			"description" => "Google reCAPTCHA v3 protection system",
+	// 			"info_link" => "https://www.google.com/recaptcha/intro/v3",
+	// 			"repos" => "https://github.com/parentnode/jam-fraudprotection-recaptcha",
+	// 		],
+	// 	]
+	// ],
 ];
 
