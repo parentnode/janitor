9fb3bc00ed3e7980f57efb7169951967d50413ca
kaestel <martin@think.dk>
2026-08-11 12:04:21 +0200

    Use shorthand

--- src/classes/items/items.core.class.php
+++ src/classes/items/items.core.class.php
@@ -349,8 +349,7 @@ class ItemsCore {
 
 			// add prices (only prices in current currency)
 			if(($all || $prices) && defined("SITE_SHOP") && SITE_SHOP) {
-				global $page;
-				$item["prices"] = $this->getPrices(array("item_id" => $item["id"], "currency" => $page->currency()));
+				$item["prices"] = $this->getPrices(array("item_id" => $item["id"], "currency" => page()->currency()));
 			}
 
 			// add subscription method (for item)
